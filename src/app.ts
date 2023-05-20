8e0e97b9e4b8d1b371aa4aa5e0c60eed2d051bea
Mohammad Hosseini <hosseini.mohamad94@gmail.com>
2023-05-20 14:42:15 +0330

    lint

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
-import express, { Request, Response } from "express";
+import express, {Express, Request, Response} from "express";
 import { Email } from "./types/Email";
 
-export const app = express();
-
-app.use(express.json())
+export const app: Express = express();
 
 app.get('/', (req: Request, res: Response): void => {
     const e: Email = new Email('foo@bar.com');
