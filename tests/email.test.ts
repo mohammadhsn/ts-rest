d3e40f13c62ced6ff92b713244f38668a7942612
Mohammad Hosseini <hosseini.mohamad94@gmail.com>
2023-05-20 14:42:08 +0330

    enable async in test cases

--- tests/email.test.ts
+++ tests/email.test.ts
@@ -2,7 +2,7 @@ import {describe, expect, test} from '@jest/globals';
 import {Email} from "../src/types/Email";
 
 describe('validate email', (): void => {
-    test('with invalid email', (): void => {
+    test('with invalid email', async () => {
         expect(() => new Email('foo')).toThrow(Error);
     });
 });
